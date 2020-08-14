4649ff15393167438f3fe887fba2d4b2672bcf8b
Robert J. Klein <robert.j.klein.phd@gmail.com>
2020-08-14 11:21:41 -0400

    Add files via upload

---
+++ stats.h
@@ -0,0 +1,30 @@
+/*
+ * stats.h
+ * 
+ * Header file for stats.c
+ */
+
+#ifndef _stats_h
+#define _stats_h
+
+void serial_make_histogram (int *gc_count, int *partitions, int num_partitions,                            CM_t *cm, int D, int num_samples, 
+			    int sample_length, float *lambda, float *K);
+
+#ifdef USE_MPI
+void parallel_make_histogram (int *gc_count, int *partitions, int num_partitions,
+			      CM_t *cm, int D, int num_samples,
+			      int sample_length, float *lambda, float *K, 
+			      int mpi_my_rank, int num_procs, 
+			      int mpi_master_rank);
+#endif
+
+void get_dbinfo (SQFILE *dbfp, long *N, int *gc_count);
+
+float e_to_score (float E, float *mu, float *lambda);
+
+double RJK_ExtremeValueE (float x, float mu, float lambda);
+
+char resolve_degenerate (char c);
+
+
+#endif
